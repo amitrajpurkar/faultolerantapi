0c3b2ed8c021f6570ce03d9c98c8bc7571020e53
Amit Rajpurkar <amit.rajpurkar@gmail.com>
2024-12-26 19:22:12 -0500

    adding one rest controller

--- src/main/java/com/anr/ftapi/FtapiApplication.java
+++ src/main/java/com/anr/ftapi/FtapiApplication.java
@@ -2,12 +2,23 @@ package com.anr.ftapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class FtapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FtapiApplication.class, args);
 	}
+
+	@GetMapping("/hi")
+	public String requestMethodName(@RequestParam( defaultValue = "world") String q) {
+		return new String("hello "+q);
+	}
+	
 
 }
